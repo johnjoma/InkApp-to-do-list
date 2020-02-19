8976e994a5821041647b7c48d08bcd978e460daa
johnjoma <irungujohnkimani@gmail.com>
2020-02-19 16:55:05 +0300

    to-do-list

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer bg-dark text-center mt-3" style="height:100px">
+<footer class="footer sticky-footer bg-dark text-center mt-3" style="height:100px">
     <div class="container">
       <span class="text-muted">Made by: John kimani &copy;2020</span>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background: rgb(2,0,36);
+                background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
